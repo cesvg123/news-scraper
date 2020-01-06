1a14bab7b6e8ae3deb1cfd54d3690c64904f1a09
Cesar Sanchez-Vegas <cesar.sanchezvegas82@gmail.com>
2020-01-06 08:08:24 -0500

    updated server.js

--- src/server.js
+++ src/server.js
@@ -7,26 +7,203 @@ var path = require("path");
 var Note = require("./models/Note");
 var Article = require("./models/Article");
 
+var request = require("request");
+var cheerio = require("cheerio");
+
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 app.use(logger(dev));
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use(express.static("./src/public/"));
+app.use(express.urlencoded({
+    extended: false
+}));
 
-var {reviews, api} = require("./src/controllers/index");
-app.use("/reviews", reviews);
-app.use("/api", api);
+app.use(express.json());
+app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/news-scraper";
 
 mongoose.connect(MONGODB_URI, {
-useUnifiedTopology: true,
-useNewUrlParser: true,
-useFindAndModify: false
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
-app.listen(PORT, () => {
-console.log(`App running on Port:` + PORT);
+
+var exphbs = require('express-handlebars');
+app.engine("handlebars", exphbs({
+    defaultLayout: "main",
+    partialsDir: path.join(__dirname, "/views/layouts/partials")
+}));
+app.set("view engine", "handlebars");
+
+db.on("error", function (error) {
+    console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function () {
+    console.log("Mongoose connection successful.");
+});
+
+app.get("/", function (req, res) {
+    Article.find({
+        "saved": false
+    }).limit(20).exec(function (error, data) {
+        var hbsObject = {
+            article: data
+        };
+        console.log(hbsObject);
+        res.render("home", hbsObject);
+    });
+});
+
+app.get("/saved", function (req, res) {
+    Article.find({
+        "saved": true
+    }).populate("notes").exec(function (error, articles) {
+        var hbsObject = {
+            article: articles
+        };
+        res.render("saved", hbsObject);
+    });
+});
+
+app.get("/scrape", function (req, res) {
+    request("https://www.nytimes.com/", function (error, response, html) {
+        var $ = cheerio.load(html);
+        $("article").each(function (i, element) {
+            var result = {};
+            result.title = $(this).children("h2").text();
+            result.link = $(this).children("h2").children("a").attr("href");
+            result.summary = $(this).children(".summary").text();
+
+            var entry = new Article(result);
+
+            entry.save(function (err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(doc);
+                }
+            });
+        });
+        res.send("Process completed");
+    });
+});
+
+app.get("/articles", function (req, res) {
+    Article.find({}).limit(10).exec(function (error, doc) {
+        if (error) {
+            console.log(error);
+        } else {
+            res.json(doc);
+        }
+    });
+});
+
+app.get("/articles/:id", function (req, res) {
+    Article.findOne({
+            "_id": req.params.id
+        })
+        .populate("note")
+        .exec(function (error, doc) {
+            if (error) {
+                console.log(error);
+            } else {
+                res.json(doc);
+            }
+        });
+});
+
+app.post("/articles/save/:id", function (req, res) {
+    Article.findOneAndUpdate({
+            "_id": req.params.id
+        }, {
+            "saved": true
+        })
+        .exec(function (err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(doc);
+                res.send(doc);
+            }
+        });
+});
+
+app.post("/articles/delete/:id", function (req, res) {
+    Article.findOneAndUpdate({
+            "_id": req.params.id
+        }, {
+            "saved": false,
+            "notes": []
+        })
+        .exec(function (err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(doc);
+                res.send(doc);
+            }
+        });
+});
+
+app.post("notes/save/:id", function (req, res) {
+    var newNote = new Note({
+        body: req.body.text,
+        article: req.params.id
+    });
+    console.log(req.body)
+    newNote.save(function (error, note) {
+        if (error) {
+            console.log(error);
+        } else {
+            Article.findOneAndUpdate({
+                    "_id": req.params.id
+                }, {
+                    $push: {
+                        "notes": note
+                    }
+                })
+                .exec(function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.send(err);
+                    } else {
+                        res.send(note);
+                    }
+                });
+        }
+    });
+});
+
+app.delete("/notes/delete/:note_id/:article", function (req, res) {
+    Note.findOneAndRemove({
+        "_id": req.params.note.id
+    }, function (err) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        } else {
+            Article.findOneAndUpdate({
+                    "_id": req.params.article_id
+                }, {
+                    $pull: {
+                        "notes": req.params.note_id
+                    }
+                })
+                .exec(function (err) {
+                    if (err) {
+                        console.log(err);
+                        res.send(err);
+                    } else {
+                        res.send("Note Deleted");
+                    }
+                });
+        }
+    });
+});
+
+app.listen(PORT, function () {
+    console.log("App running on PORT: " + PORT);
 });
